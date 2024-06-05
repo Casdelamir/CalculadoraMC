514a558a84b51fbde13e630c0bed1cb7d1668e7f
mananas <casdelamir@gmail.com>
2024-06-05 09:32:59 +0200

    New button

--- app/src/main/java/com/example/calculadoramc/MainActivity.kt
+++ app/src/main/java/com/example/calculadoramc/MainActivity.kt
@@ -2,10 +2,23 @@ package com.example.calculadoramc
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.LinearLayout
 
 class MainActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
+
+    lateinit var containerView : LinearLayout
+    lateinit var button: Button
+            override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-    }
+
+                containerView = findViewById(R.id.containerView)
+
+                containerView.setBackgroundResource(R.color.red)
+
+                button = findViewById(R.id.button)
+
+                button.setOnClickListener { containerView.setBackgroundResource(com.example.calculadoramc.R.color.black) }
+            }
 }
