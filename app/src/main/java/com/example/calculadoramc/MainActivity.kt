14581afe726875987cfb48692c73e3f5f0f70f18
mananas <casdelamir@gmail.com>
2024-06-05 13:58:45 +0200

    IBM calculator first version

--- app/src/main/java/com/example/calculadoramc/MainActivity.kt
+++ app/src/main/java/com/example/calculadoramc/MainActivity.kt
@@ -1,24 +1,61 @@
 package com.example.calculadoramc
 
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
-import android.widget.LinearLayout
+import android.widget.EditText
+import android.widget.TextView
+import kotlin.math.pow
 
 class MainActivity : AppCompatActivity() {
 
-    lateinit var containerView : LinearLayout
-    lateinit var button: Button
-            override fun onCreate(savedInstanceState: Bundle?) {
+    lateinit var hightText: EditText
+    lateinit var weightText: TextView
+    lateinit var addButton: Button
+    lateinit var restButton: Button
+    lateinit var calculateButton: Button
+    lateinit var result: TextView
+
+    var hight = 200
+    var weight = 140
+    override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-                containerView = findViewById(R.id.containerView)
+        hightText = findViewById(R.id.valueHeight)
+        weightText = findViewById(R.id.valueWeight)
+        addButton = findViewById(R.id.addButton)
+        restButton = findViewById(R.id.restButton)
+        calculateButton = findViewById(R.id.calculate)
+        result = findViewById(R.id.result)
 
-                containerView.setBackgroundResource(R.color.red)
+        setHeight()
+        setWeight()
 
-                button = findViewById(R.id.button)
+        addButton.setOnClickListener {
+            weight++
+            setWeight()
+        }
 
-                button.setOnClickListener { containerView.setBackgroundResource(com.example.calculadoramc.R.color.black) }
-            }
+        restButton.setOnClickListener {
+            weight--
+            setWeight()
+        }
+
+        calculateButton.setOnClickListener {
+            hight = hightText.text.toString().toInt()
+            val calc = weight / (hight / 100f).pow(2)
+            result.text = calc.toString()
+        }
+    }
+
+    fun setHeight() {
+        hightText.setText(hight.toString())
+    }
+
+
+    fun setWeight() {
+        weightText.text = "$weight Kg"
+    }
 }
