a390412a3df1e1cf770c984a24a3f8e862f0c5b5
mananas <casdelamir@gmail.com>
2024-06-06 14:00:59 +0200

    slider added and classification added

--- app/src/main/java/com/example/calculadoramc/MainActivity.kt
+++ app/src/main/java/com/example/calculadoramc/MainActivity.kt
@@ -1,10 +1,14 @@
 package com.example.calculadoramc
 
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
+import com.google.android.material.slider.Slider
 import kotlin.math.pow
 
 class MainActivity : AppCompatActivity() {
@@ -16,9 +20,15 @@ class MainActivity : AppCompatActivity() {
     lateinit var calculateButton: Button
     lateinit var result: TextView
     lateinit var resultClassification: TextView
+    lateinit var slider: Slider
 
-    var hight = 200
+    val handler = Handler(Looper.getMainLooper())
+    lateinit var runnableSum: Runnable
+    lateinit var runnableRest: Runnable
+
+    var hight = 150
     var weight = 60
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -30,29 +40,61 @@ class MainActivity : AppCompatActivity() {
         calculateButton = findViewById(R.id.calculate)
         result = findViewById(R.id.result)
         resultClassification = findViewById(R.id.result_classification)
+        slider = findViewById(R.id.slider)
 
         setHeight()
         setWeight()
 
-        addButton.setOnClickListener {
-            weight++
-            setWeight()
+        runnableSum = object : Runnable {
+            override fun run() {
+                weight++
+                setWeight()
+                handler.postDelayed(this, 100)
+            }
         }
 
-        restButton.setOnClickListener {
-            weight--
-            setWeight()
+        runnableRest = object : Runnable {
+            override fun run() {
+                weight--
+                setWeight()
+                handler.postDelayed(this, 100)
+            }
         }
+
+        clickButton(runnableSum, addButton)
+        clickButton(runnableRest, restButton)
 
         calculateButton.setOnClickListener {
             hight = hightText.text.toString().toInt()
             val calc = weight / (hight / 100f).pow(2)
-            result.text = calc.toString()
-            serResultClassification()
+            result.text = calc.toString().format("%.${2}f")
+            setResultClassification()
+        }
+
+        slider.addOnChangeListener { _, value, _ ->
+            hight = value.toInt()
+            setHeight()
         }
     }
 
-    fun serResultClassification() {
+    @SuppressLint("ClickableViewAccessibility")
+    fun clickButton(runnable: Runnable, button: Button) {
+        button.setOnTouchListener { _, event ->
+            when (event.action) {
+                android.view.MotionEvent.ACTION_DOWN -> {
+                    handler.post(runnable)
+                    true
+                }
+                android.view.MotionEvent.ACTION_UP, android.view.MotionEvent.ACTION_CANCEL -> {
+                    handler.removeCallbacks(runnable)
+                    true
+                }
+                else -> false
+            }
+        }
+    }
+
+    fun setResultClassification() {
         val resultNum = result.text.toString().toFloat()
         if (resultNum < 18.5) {
             resultClassification.setText(R.string.result_classification_low)
@@ -69,8 +111,10 @@ class MainActivity : AppCompatActivity() {
 
     fun setHeight() {
         hightText.setText(hight.toString())
+        slider.value = hight.toFloat()
     }
 
+    @SuppressLint("SetTextI18n")
     fun setWeight() {
         weightText.text = "$weight Kg"
     }
